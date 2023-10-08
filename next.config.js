280e0ae740bf1e84ba7d38097fb8ba5f3962a900
Moyez <moyezrabbani@gmail.com>
2023-10-08 19:59:39 +0530

    updated next config for sign-in and sign-up redirects

--- next.config.js
+++ next.config.js
@@ -2,10 +2,23 @@ const { config } = require('process')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirect() {
+        return [
+            {
+                source:"/sign-in",
+                destination: "/api/auth/login",
+                permanent: true
+            },
+            {
+                source: "/sign-up",
+                destination: "/api/auth/register",
+                permanent: true
+            }
+        ]
+    },
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         config.resolve.alias.canvas = false
         config.resolve.alias.encoding = false;
-
         return config;
     }
 
